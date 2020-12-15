47b4058a35cfa392269706b938c62b365b479207
Astropal <44496245+Astropal@users.noreply.github.com>
2020-12-15 16:14:09 +0100

    - Add Files base -

---
+++ HiDEV_LoadingScreen/__resource.lua
@@ -0,0 +1,14 @@
+version '1.0.0'
+author 'Astropal - HiDEV'
+description 'HiDEV loading screen.'
+repository 'https://github.com/Astropal/HiDEV_LoadingScreen'
+
+files {
+    'index.html',
+    'src/CSS/style.css',
+    'src/IMG/loadscreen.jpg'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
